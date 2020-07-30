2b89217991a28147ceab3fb2113c1d41316d2c93
solus9993 <56090427+solus9993@users.noreply.github.com>
2020-07-30 21:20:15 +0100

    changed URL

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ var config = {
     mongodb: 'mongodb+srv://mongo_user:mongo_pass@cluster0-oswkr.gcp.mongodb.net/project?retryWrites=true&w=majority',
     secret: 'sdkljhv93048hfv89ren',
     port: 3000,
-    baseURL: "http://localhost",
+    // baseURL: "http://localhost",
+    baseURL: "https://tracker-app-api.herokuapp.com/",
     // baseURL: "http://testcenas.ddns.net",
     cronZoneCount: true,
     permissions:{
